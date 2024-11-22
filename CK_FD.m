7fdb118f8b4a912229cad971321952428180191d
Sara Kamali <61450811+Sara-Kamali@users.noreply.github.com>
2024-11-22 16:07:57 +0100

    Update CK_FD.m

--- CK_FD.m
+++ CK_FD.m
@@ -4,26 +4,46 @@
 
 function CKFD= CK_FD(y)
 
-N = length(y);
 
 %Computing the difference between the max and min of the vector
-max_dist = abs(max(y)- min(y));
+max_dist = abs(max(y)- min(y)); 
 
 % Find the size of sliding window
-N_win= max(max_dist,8);
+% Initialize variables
+N = length(y);
+window_length = N; % Start with the full length, then reduce
+
+% Iterate over possible window lengths
+for w = floor(N/2):-1:8
+    dist_yw=zeros(1,N-w);
+    % Slide the window across the signal
+    for i = 1:(N - w + 1)
+        yw = y(i:i+w-1); % Current window
+        dist_yw(i) = abs(max(yw) - min(yw));
+    end
+    if (max(dist_yw)) <= max_dist/2
+        % If condition is met stop searching
+        break
+    end
+end
+window_length = w;
+
+
+
 % Size of overlap between windows, set to 20%
-N_ovrlap = ceil(N_win * .2);
+N_ovrlap = ceil(window_length * .2);
 
 j=0;CKFD_win=[];
 % Slide the window
-for i=1:N_win-N_ovrlap:N-N_win
-    yw=y(i:i+N_win);
+for i=1:window_length-N_ovrlap:N-window_length
+    yw=y(i:i+window_length);
     % Sum the absolute difference between consecutive poits
     eu_length = sum(abs(diff(yw)));
+
     max_dist_yw= abs(max(yw)-min(yw));
     % Compute CKFD of the current window
     j=j+1;
-    CKFD_win(j) = log10(N_win)/(log10(N_win) + log10(max_dist_yw/eu_length));
+    CKFD_win(j) = log10(window_length)/(log10(window_length) + log10(max_dist_yw/eu_length)); 
 end
 
 CKFD= mean(CKFD_win);
