a9e1bc5817a7d3d7d91d1e4bffb9a2def3fd05a0
Sara Kamali <61450811+Sara-Kamali@users.noreply.github.com>
2025-01-24 09:51:20 +0100

    Castiglioni_FD.m

--- CK_FD.m
+++ CK_FD.m
@@ -2,8 +2,7 @@
 %call it CKFD. (DOI: https://doi.org/10.1016/j.compbiomed.2010.10.001)
 % The input, y, is a one dimensional time-series vector.
 
-function CKFD= CK_FD(y)
-
+function CFD= Castiglioni_FD(y)
 
 %Computing the difference between the max and min of the vector
 max_dist = abs(max(y)- min(y)); 
@@ -30,8 +29,8 @@ window_length = w;
 
 
 
-% Size of overlap between windows, set to 20%
-N_ovrlap = ceil(window_length * .2);
+% Size of overlap between windows, set to 70%
+N_ovrlap = ceil(window_length * .7);
 
 j=0;CKFD_win=[];
 % Slide the window
@@ -39,16 +38,11 @@ for i=1:window_length-N_ovrlap:N-window_length
     yw=y(i:i+window_length);
     % Sum the absolute difference between consecutive poits
     eu_length = sum(abs(diff(yw)));
-
     max_dist_yw= abs(max(yw)-min(yw));
     % Compute CKFD of the current window
     j=j+1;
     CKFD_win(j) = log10(window_length)/(log10(window_length) + log10(max_dist_yw/eu_length)); 
 end
 
-CKFD= mean(CKFD_win);
-
-
-
-
+CFD= mean(CKFD_win);
 
